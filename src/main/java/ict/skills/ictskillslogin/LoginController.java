c77c3a9d171d37426ac60a4a61b3916485b79731
lionelm <lionel.mueller@quickline.ch>
2023-09-15 11:55:37 +0200

    navigation wip

--- src/main/java/ict/skills/ictskillslogin/LoginController.java
+++ src/main/java/ict/skills/ictskillslogin/LoginController.java
@@ -1,5 +1,6 @@
 package ict.skills.ictskillslogin;
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -7,19 +8,31 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.util.Objects;
 
-public class MainMenuViewController {
+public class LoginController {
     @FXML
-    protected void onLogout(ActionEvent event) throws IOException {
-        Parent fxmlView = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("login-view.fxml")));
+    private TextField loginUsernameField;
+    @FXML
+    private Button loginSubmitButton;
+    @FXML
+    private Hyperlink loginCancelLink;
+
+    @FXML
+    protected void onLoginSubmit(ActionEvent event) throws IOException {
+        Parent fxmlView = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("main-menu-view.fxml")));
         Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
         Scene scene = new Scene(fxmlView);
         stage.setScene(scene);
         stage.show();
+    }
+    @FXML
+    protected void onLoginCancel(ActionEvent event) {
+        Platform.exit();
     }
 }
